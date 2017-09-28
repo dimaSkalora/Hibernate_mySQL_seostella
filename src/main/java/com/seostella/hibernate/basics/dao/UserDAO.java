29b3f8752957183acf6360cb9b6e6d7d67abfd82
dimaSkalora <timon2@ukr.net>
2017-09-28 12:26:21 +0300

    UserDAO

---
+++ src/main/java/com/seostella/hibernate/basics/dao/UserDAO.java
@@ -0,0 +1,48 @@
+package com.seostella.hibernate.basics.dao;
+
+import com.seostella.hibernate.basics.entity.User;
+import org.hibernate.HibernateException;
+import org.hibernate.query.Query;
+
+
+public class UserDAO extends DAO {
+    public User createrUser(String username, String password) throws Exception{
+       try{
+           begin();
+           User user = new User(username,password);
+           getSession().save(user); // сохранение
+           commit();
+           return user;
+       }catch (HibernateException e){
+           rolback();
+           throw new Exception("Could not create user "+username,e);
+       }
+    }
+
+    public User retrieveUser(String username)throws Exception{
+        try{
+            begin();
+            Query q = getSession().createQuery("FROM User WHERE name = :username"); // поиск
+            q.setString("username",username);
+            User user = (User) q.uniqueResult();
+            commit();
+            return user;
+        }catch (HibernateException e){
+            rolback();
+            throw new Exception("Could not get user "+username,e);
+        }
+    }
+
+    public void deleteUser(User user) throws Exception{
+        try{
+            begin();
+            getSession().delete(user); // удаление
+            commit();
+        }catch (HibernateException e){
+            rolback();
+            throw new Exception("Could not delete user "+user.getName(),e);
+        }
+    }
+
+
+}
